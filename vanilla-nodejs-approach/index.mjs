0a7440025d4d6c191337e4274b445b2db7ad128b
userAugustos <felipe_augustos84@outlook.com.br>
2023-01-31 10:04:20 -0300

    starting mjs server, with an simple endpoint

--- vanilla-nodejs-approach/index.mjs
+++ vanilla-nodejs-approach/index.mjs
@@ -1,1 +1,16 @@
-console.debug('hello world')
+import { createServer } from 'http';
+
+const PORT = 2009
+
+createServer((req, res) => {
+	res.writeHead(200);
+	res.end("You're on a Vanilla approach of websockets with nodejs")
+}).listen(PORT, () => console.debug(`all good to go in localhost:${PORT}`));
+
+
+;[
+	"uncaughtException",
+	"unhandledRejection"
+].forEach(event => {
+	process.on(event, (err) => console.error(`error on ${event}; ${err.stack || err}`))
+})
