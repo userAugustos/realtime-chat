d873f60d19df0eda7838a33abdea7b7104d216df
userAugustos <felipe_augustos84@outlook.com.br>
2023-02-19 12:21:47 -0300

    chore: initiating work reading the bytes

--- vanilla-nodejs-approach/index.mjs
+++ vanilla-nodejs-approach/index.mjs
@@ -1,9 +1,14 @@
 import { createServer } from 'http';
 import crypto from 'crypto';
 
-const WEBSOCKET_MAGIC_STRING_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
-
 const PORT = 2009
+
+const WEBSOCKET_MAGIC_STRING_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+const SEVEN_BITES_INTEGER_MARKER = 125
+const SIXTEEN_BITES_INTEGER_MARKER = 126
+const SIXTYFOUR_BITS_INTEGER_MARKER = 127 // 127 is the maximum we can work on a data frame
+const FIRST_BIT = 128
+
 
 const server = createServer((req, res) => {
 	res.writeHead(200);
@@ -28,7 +33,7 @@ function prepareHandShakeResponse(id) {
 		'Upgrade: websocket',
 		'Connection: Upgrade',
 		`Sec-WebSocket-Accept: ${acceptKey}`,
-		'' // tihs empty line must be present to response be valid
+		'' // this empty line must be present to response be valid
 	].map(line => line.concat('\r\n')).join('')
 }
 
@@ -41,8 +46,34 @@ function onSocketUpgrade (req, socket, head) {
 	socket.on('readable', () => onSocketReadable(socket))
 }
 
-function onSocketReadable(socket) {
+/*
+[
+  1, // mask bit
+  // payload starts here
+  0, // bit 1
+  0, // bit 2
+  0, // bit 3
+  1, // bit 4
+  0, // bit 5
+  1, // bit 6
+  1  // bit 7
+  // total value of these bits tell us if payload is here or on the next byte
+]
+* */
 
+function onSocketReadable(socket) {
+	socket.read(1) // read and from moment, does nothing
+
+	// so now we are in the second byte
+	const [markerAndPayLoadLength] = socket.read(1) // passing 1, can look weirdo, but, the method read() of socket, read we pass the byte to read, so whe nhe reads, it's discarded
+	// so after we make socket.read(3) the 3 byte will be read, or stored, and then discarded, so, when we read the first one in line 63, and didn't store it, he doesn't "exist" more
+
+	console.debug("initial", markerAndPayLoadLength)
+	console.debug("to byte", (markerAndPayLoadLength).toString(2))
+
+	const lengthIndicatorInBits = markerAndPayLoadLength - FIRST_BIT
+
+	let messageLength = 0
 }
 ;[
 	"uncaughtException",
